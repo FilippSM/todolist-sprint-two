2c9b2f8361f742c51e40adbd2c9f67b3e1f3d122
Filipp Sam <philippsm666@gmail.com>
2025-01-21 20:03:32 +0300

    (sprint 2 week 4) reducer unit

--- src/model/todolists-reducer.ts
+++ src/model/todolists-reducer.ts
@@ -1,20 +1,43 @@
 import { Todolist } from "../App";
 
-export type DeleteTodolistActionType = {
-  type: "delete_todolist";
-  payload: {
-    id: string;
-  };
-};
+type ActionType = DeleteTodolistActionType | createTodolistActionType | ChangeTodolistTitleActionType
 
-export const todolistsReducer = (
-  todolists: Todolist[],
-  action: DeleteTodolistActionType
-): Todolist[] => {
+// Reducer
+export const todolistsReducer = (todolists: Todolist[], action: ActionType): Todolist[] => {
   switch (action.type) {
-    case "delete_todolist":
-      return todolists.filter(todolist => todolist.id !== action.payload.id)
+    case "delete_todolist": {
+      const { id } = action.payload
+      return todolists.filter(todolist => todolist.id === id)
+    }
+    case "create_todolist": {
+      const { id, title } = action.payload
+      return [...todolists, { id, title, filter: 'all' }]
+    }
+    case "change_todolist_title": {
+      const { id, title } = action.payload
+      return todolists.map(todolist => todolist.id === id ? { ...todolist, title } : todolist)
+    }
     default:
-    return todolists;
+      return todolists;
   }
 };
+
+// Action Creator
+export const DeleteTodolistAC = (id: string) => {
+  return { type: "delete_todolist", payload: { id } } as const; // Добавлен return
+};
+
+// Action Type
+export type DeleteTodolistActionType = ReturnType<typeof DeleteTodolistAC>;
+
+
+export const CreateTodolistAC = (title: string, id: string) => (
+  { type: "create_todolist", payload: { title, id } } as const
+)
+
+export type createTodolistActionType = ReturnType<typeof CreateTodolistAC>
+
+export const ChangeTodolistTitleAC = (payload: { id: string, title: string }) => (
+  { type: "change_todolist_title", payload } as const
+)
+export type ChangeTodolistTitleActionType = ReturnType<typeof ChangeTodolistTitleAC>
